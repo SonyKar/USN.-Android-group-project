8e126687e8333a004ff8f56551b19d6c1d9dfaea
CosminaL <cosmina_lucaci@yahoo.com>
2021-10-23 15:44:04 +0300

    Register Page (Sign Up): added Status validation Create Course Page: design + validations

---
+++ app/src/main/java/com/example/mobproject/CreateCourse.java
@@ -0,0 +1,262 @@
+package com.example.mobproject;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+public class CreateCourse extends AppCompatActivity implements DatePickerDialog.OnDateSetListener{
+
+
+    private TextView startDate, endDate;
+    private EditText createCourseName, createCoursePrice, description;
+    private boolean validCreateCourseName, validCreateCoursePrice, validCreateCourseDates,
+            validCreateCourseDifficulty, validCreateCourseDays, validCreateCourseDescription;
+    private Spinner categorySpinner;
+    private ImageButton startDateBtn, endDateBtn;
+    private Integer StartEndContor, checkedID;
+    private ArrayList<Integer> daysChecked = new ArrayList<Integer>();
+    private Button createCourse;
+    private Date date1, date2;
+    private RadioGroup difficulty;
+    private RadioButton selectedDifficulty;
+    private CheckBox Monday, Tuesday, Wednesday, Thursday, Friday, Saturday;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_course);
+
+        createCourseName = (EditText) findViewById(R.id.create_course_name);
+        createCoursePrice = (EditText) findViewById(R.id.create_course_price);
+        categorySpinner = (Spinner) findViewById(R.id.category_spn);
+        startDateBtn = (ImageButton) findViewById(R.id.start_date_btn);
+        endDateBtn = (ImageButton) findViewById(R.id.end_date_btn);
+        startDate = (TextView) findViewById(R.id.start_date);
+        endDate = (TextView) findViewById(R.id.end_date);
+        createCourse = (Button) findViewById(R.id.create_course_btn);
+        difficulty = (RadioGroup) findViewById(R.id.difficulty_radgr);
+        Monday = (CheckBox) findViewById(R.id.monday_chck);
+        Tuesday = (CheckBox) findViewById(R.id.tuesday_chck);
+        Wednesday = (CheckBox) findViewById(R.id.wedn_chck);
+        Thursday = (CheckBox) findViewById(R.id.thurs_chck);
+        Friday = (CheckBox) findViewById(R.id.friday_chck);
+        Saturday = (CheckBox) findViewById(R.id.saturday_chck);
+        description = (EditText) findViewById(R.id.description_box);
+
+
+        startDateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                StartEndContor = 1;
+                showDatePickerDialog();
+
+            }
+        });
+
+        endDateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                StartEndContor = 2;
+                showDatePickerDialog();
+
+            }
+        });
+
+
+
+        ArrayAdapter<String> categoriesAdapter = new ArrayAdapter<String>(CreateCourse.this,
+                android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.categories));
+        categoriesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        categorySpinner.setAdapter(categoriesAdapter);
+
+
+        createCourse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createCourseValidation();
+            }
+        });
+
+    }
+
+    private void createCourseValidation() {
+        //validate course Name
+        if(createCourseName.getText().toString().isEmpty()){
+            createCourseName.setError(getResources().getString(R.string.first_name_error));
+            validCreateCourseName = false;
+        }
+        else
+            validCreateCourseName = true;
+
+        //validate course Price
+        if(createCoursePrice.getText().toString().isEmpty()){
+            createCoursePrice.setError(getResources().getString(R.string.price_error));
+            validCreateCoursePrice = false;
+        }
+        else
+            validCreateCoursePrice = true;
+
+        //validate Start Date
+        if(startDate.getText().toString().isEmpty()){
+            startDate.setError(getResources().getString(R.string.start_date_error));
+            //Toast.makeText(getApplicationContext(), R.string.chronology_error, Toast.LENGTH_SHORT).show();
+            validCreateCourseDates = false;
+        }
+        //validate End Date
+       else
+           if (endDate.getText().toString().isEmpty()) {
+                endDate.setError(getResources().getString(R.string.end_date_error));
+                //Toast.makeText(getApplicationContext(), R.string.chronology_error, Toast.LENGTH_SHORT).show();
+                validCreateCourseDates = false;
+            }
+
+           //validate Date chronology
+
+            else if(date1.after(date2) || date1.equals(date2)){
+                //startDate.setError(getResources().getString(R.string.chronology_error));
+                //endDate.setError(getResources().getString(R.string.chronology_error));
+                Toast.makeText(getApplicationContext(), R.string.chronology_error, Toast.LENGTH_SHORT).show();
+                validCreateCourseDates = false;
+            }
+
+            else
+                validCreateCourseDates = true;
+
+            //validate Difficulty
+        if(difficulty.getCheckedRadioButtonId() == -1)
+        {
+            Toast.makeText(getApplicationContext(), R.string.difficulty_error, Toast.LENGTH_SHORT).show();
+            validCreateCourseDifficulty = false;
+        }
+        else
+        {
+            validCreateCourseDifficulty = true;
+            // get selected radio button from radioGroup
+            int selectedId = difficulty.getCheckedRadioButtonId();
+
+            // find the radiobutton by returned id
+
+            selectedDifficulty = (RadioButton)findViewById(selectedId);
+           // Toast.makeText(getApplicationContext(), selectedDifficulty.getText().toString()+" is selected", Toast.LENGTH_SHORT).show();
+        }
+
+
+        //validate Days
+        //get Days
+        if(Monday.isChecked()) {
+            checkedID = 1;
+            daysChecked.add(checkedID);
+        }
+        else if(Tuesday.isChecked()){
+            checkedID = 2;
+            daysChecked.add(checkedID);
+        }
+        else if(Wednesday.isChecked()){
+            checkedID = 3;
+            daysChecked.add(checkedID);
+        }
+        else if(Thursday.isChecked()){
+            checkedID = 4;
+            daysChecked.add(checkedID);
+        }
+        else if(Friday.isChecked()){
+            checkedID = 5;
+            daysChecked.add(checkedID);
+        }
+        else if(Tuesday.isChecked()){
+            checkedID = 6;
+            daysChecked.add(checkedID);
+        }
+
+        if(daysChecked.size() > 0)
+            validCreateCourseDays = true;
+        else {
+            Toast.makeText(getApplicationContext(), R.string.days_error, Toast.LENGTH_SHORT).show();
+            validCreateCourseDays = false;
+        }
+
+
+        //validate Description
+        if(description.getText().toString().isEmpty()){
+            description.setError(getResources().getString(R.string.description_error));
+            validCreateCourseDescription = false;
+        }
+        else
+            validCreateCourseDescription = true;
+
+        //validate
+        if(validCreateCourseName && validCreateCoursePrice && validCreateCourseDates && validCreateCourseDifficulty
+        && validCreateCourseDays && validCreateCourseDescription)
+            Toast.makeText(getApplicationContext(), R.string.course_created_mess, Toast.LENGTH_SHORT).show();
+
+    }
+
+
+
+
+
+
+    private void showDatePickerDialog(){
+        DatePickerDialog datePickerDialog =
+                new DatePickerDialog(
+                        this,
+                        R.style.DatePicker,
+                        this,
+                        Calendar.getInstance().get(Calendar.YEAR),
+                        Calendar.getInstance().get(Calendar.MONTH),
+                        Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+    );
+        datePickerDialog.show();
+
+
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        String date = dayOfMonth + "/" + month + "/" + year;
+        if(StartEndContor == 1){ //start date
+            try {
+                date1 = new SimpleDateFormat("dd/MM/yyyy").parse(date);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            startDate.setText(date);
+        }
+
+
+        else
+            if(StartEndContor == 2){//end date
+                try {
+                    date2 = new SimpleDateFormat("dd/MM/yyyy").parse(date);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                endDate.setText(date);
+            }
+
+
+    }
+
+}
--- app/src/main/java/com/example/mobproject/RegisterActivity.java
+++ app/src/main/java/com/example/mobproject/RegisterActivity.java
@@ -6,6 +6,7 @@ import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
@@ -16,7 +17,8 @@ public class RegisterActivity extends AppCompatActivity {
     EditText signupEmail, signupFirstName, signupLastName, signupPass, signupConfPass;
     Button register, btnToLogin;
     RadioGroup status;
-    Boolean validSignUpEmail, validSignUpPassword, validSignUpFirstName, validSignUpLastName;
+    Boolean validSignUpEmail, validSignUpPassword, validSignUpFirstName, validSignUpLastName, validSignUpStatus;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +33,12 @@ public class RegisterActivity extends AppCompatActivity {
         register = (Button) findViewById(R.id.signup_btn);
         status = (RadioGroup) findViewById(R.id.rad_status);
         btnToLogin = (Button) findViewById(R.id.btn_to_login);
+
+        /* get selected radio button from radioGroup
+        int selectedId = gender.getCheckedRadioButtonId();
+        // find the radiobutton by returned id
+        selectedRadioButton = (RadioButton)findViewById(selectedId);
+        Toast.makeText(getApplicationContext(), selectedRadioButton.getText().toString()+" is selected", Toast.LENGTH_SHORT).show();*/
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,7 +60,7 @@ public class RegisterActivity extends AppCompatActivity {
         Boolean passConfirm = signupPass.getText().toString().equals(
                 signupConfPass.getText().toString());
 
-        //Check signup first name
+        //validate signup FIRST NAME
         if(signupFirstName.getText().toString().isEmpty()){
             signupFirstName.setError(getResources().getString(R.string.first_name_error));
             validSignUpFirstName = false;
@@ -60,7 +68,7 @@ public class RegisterActivity extends AppCompatActivity {
         else
             validSignUpFirstName = true;
 
-        //Check signup last name
+        //validate signup LAST NAME
         if(signupLastName.getText().toString().isEmpty()){
             signupLastName.setError(getResources().getString(R.string.last_name_error));
             validSignUpLastName = false;
@@ -68,7 +76,7 @@ public class RegisterActivity extends AppCompatActivity {
         else
             validSignUpLastName = true;
 
-        //Check signup email address
+        //validate signup EMAIL ADDRESS
         if(signupEmail.getText().toString().isEmpty()){
             signupEmail.setError(getResources().getString(R.string.email_error));
             validSignUpEmail = false;
@@ -79,7 +87,7 @@ public class RegisterActivity extends AppCompatActivity {
             validSignUpEmail = true;
         }
 
-        //Check signup password
+        //validate signup PASSWORD
         if(signupPass.getText().toString().isEmpty()){
             signupPass.setError(getResources().getString(R.string.password_error));
             validSignUpPassword = false;
@@ -95,8 +103,32 @@ public class RegisterActivity extends AppCompatActivity {
         else
             validSignUpPassword = true;
 
+        //validate STATUS
+        if (status.getCheckedRadioButtonId() == -1)
+        {
+            // no radio buttons are checked
+            Toast.makeText(getApplicationContext(),
+                    R.string.status_error,
+                    Toast.LENGTH_SHORT).show();
+            validSignUpStatus = false;
+        }
+        else
+        {
+            // one of the radio buttons is checked
+            validSignUpStatus = true;
 
-        if(validSignUpFirstName && validSignUpLastName && validSignUpEmail && validSignUpPassword){
+            // get selected radio button from radioGroup
+            int selectedStatusId = status.getCheckedRadioButtonId();
+
+            // find the radiobutton by returned id
+
+            //selectedStatus = (RadioButton)findViewById(selectedId);
+
+        }
+
+
+        if(validSignUpFirstName && validSignUpLastName && validSignUpEmail && validSignUpPassword
+                && validSignUpStatus){
             Toast.makeText(getApplicationContext(),
                     "Successful Signup!!",
                     Toast.LENGTH_SHORT).show();
