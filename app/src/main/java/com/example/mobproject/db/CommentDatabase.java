19cfc959cc1954bc2ed90f80ddebe1e42b33534b
SonyKar <sa.caraganciu@gmail.com>
2021-11-16 15:46:48 +0100

    - Read comments from database

---
+++ app/src/main/java/com/example/mobproject/db/CommentDatabase.java
@@ -0,0 +1,66 @@
+package com.example.mobproject.db;
+
+import com.example.mobproject.constants.DatabaseCollections;
+import com.example.mobproject.constants.ErrorCodes;
+import com.example.mobproject.constants.ErrorMessages;
+import com.example.mobproject.interfaces.Callback;
+import com.example.mobproject.models.Comment;
+import com.example.mobproject.models.Error;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+
+public class CommentDatabase extends Database<Comment> {
+
+    @Override
+    public void getItem(String id, Callback<Comment> callback) {
+
+    }
+
+    @Override
+    public void getItems(Callback<Comment> callback) {
+        db.collection(DatabaseCollections.COURSES_COLLECTION).get().addOnSuccessListener(snapshots -> {
+            ArrayList<Comment> commentsList = new ArrayList<>();
+            for (QueryDocumentSnapshot document : snapshots) {
+                Comment tmp = document.toObject(Comment.class);
+                tmp.setId(document.getId());
+                commentsList.add(tmp);
+            }
+            callback.OnFinish(commentsList);
+        });
+    }
+
+    @Override
+    public Error insertItem(Comment item) {
+        Error error;
+        DocumentReference newCommentRef = db.collection(DatabaseCollections.COURSES_COLLECTION).document();
+
+        if ((error = validateItem(item)) != null) {
+            return error;
+        }
+
+        newCommentRef.set(item);
+
+        return null;
+    }
+
+    @Override
+    public Error updateItem(String id, Comment item) {
+        return null;
+    }
+
+    @Override
+    public Error removeItem(String id) {
+        return null;
+    }
+
+    @Override
+    public Error validateItem(Comment item) {
+        if (item.getCommentText().trim().isEmpty()) {
+            return new Error(ErrorCodes.EMPTY_FIELDS, ErrorMessages.EMPTY_FIELDS);
+        }
+
+        return null;
+    }
+}
