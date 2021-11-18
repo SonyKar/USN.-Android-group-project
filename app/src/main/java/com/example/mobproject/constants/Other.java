1e2e727a5e5c7b1819bcaf5747f9bdf53c2ccfa9
CosminaL <cosmina_lucaci@yahoo.com>
2021-11-18 16:10:39 +0200

    CourseProfile: - edit course btn invisible for Student
    MenuDrawer:
    - createCourse btn invisible for Student
    
    Other Constants:
    - changed Other.sharedPrefFile: com.example.mobproject_preferences

--- app/src/main/java/com/example/mobproject/constants/Other.java
+++ app/src/main/java/com/example/mobproject/constants/Other.java
@@ -2,7 +2,7 @@ package com.example.mobproject.constants;
 
 public class Other {
 
-    public static final String sharedPrefFile = "com.example.mobproject";
+    public static final String sharedPrefFile = "com.example.mobproject_preferences";
     public static final String SHARED_PREF_USERID = "id";
     public static final String SHARED_PREF_USERTYPE = "userType";
     public static final String SHARED_PREF_NODATA = "No data";
