ae4a6a3c0abd2c54be0274ecfae5e46facf8ad92
SonyKar <sa.caraganciu@gmail.com>
2021-11-18 15:00:10 +0100

    - MenuDrawer optimized

--- app/src/main/java/com/example/mobproject/MenuDrawer.java
+++ app/src/main/java/com/example/mobproject/MenuDrawer.java
@@ -1,59 +1,57 @@
 package com.example.mobproject;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.text.Layout;
-import android.view.MenuItem;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
-import androidx.fragment.app.FragmentManager;
 
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MenuDrawer {
 
-
-
     public static void setupDrawerContent(NavigationView navigationView, Context context, DrawerLayout drawer) {
+        final int PROFILE = R.id.nav_profile;
+        final int COURSES = R.id.nav_courses;
+        final int ENROLLED_COURSES = R.id.nav_my_courses;
+        final int FAVOURITES_COURSES = R.id.nav_fav;
+        final int CREATE_COURSE = R.id.nav_create_course;
+        final int LOG_OUT = R.id.nav_log_out;
+
         navigationView.setNavigationItemSelectedListener(
                 menuItem -> {
                     switch(menuItem.getItemId()){
-                        case R.id.nav_profile:
+                        case PROFILE:
                             Toast.makeText(context, "Profile clicked", Toast.LENGTH_SHORT).show();
                             Intent toProfile = new Intent(context, UserProfile.class);
                             toProfile.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                             context.startActivity(toProfile);
                             break;
-                        case R.id.nav_courses:
+                        case COURSES:
                             Intent toCourseList = new Intent(context, CourseList.class);
                             toCourseList.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                             context.startActivity(toCourseList);
                             break;
-                        case R.id.nav_my_courses:
+                        case ENROLLED_COURSES:
                             Toast.makeText(context, "MyCourses clicked", Toast.LENGTH_SHORT).show();
                             Intent toMyCourses = new Intent(context, MyCoursesList.class);
                             toMyCourses.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                             context.startActivity(toMyCourses);
                             break;
-                        case R.id.nav_fav:
+                        case FAVOURITES_COURSES:
                             //Toast.makeText(context, "Favourites clicked", Toast.LENGTH_SHORT).show();
                             Intent toFavourites = new Intent(context, FavouriteList.class);
                             toFavourites.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                             context.startActivity(toFavourites);
                             break;
-                        case R.id.nav_create_course:
+                        case CREATE_COURSE:
                             Intent toCreateCourse = new Intent(context, CreateCourse.class);
                             toCreateCourse.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                             context.startActivity(toCreateCourse);
                             break;
-                        case R.id.nav_log_out:
+                        case LOG_OUT:
                             FirebaseAuth.getInstance().signOut();
                             Intent toLogin = new Intent(context, LoginActivity.class);
                             toLogin.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -66,83 +64,4 @@ public class MenuDrawer {
                     return true;//the item was selected
                 });
     }
-
-//    private static void selectDrawerItem(MenuItem menuItem) {
-//        Fragment fragment = null;
-//        Class fragmentClass;
-//
-//        switch(menuItem.getItemId()) {
-//            case R.id.nav_profile:
-//                fragmentClass = UserProfile.class;
-//                break;
-//            case R.id.nav_courses:
-//                fragmentClass = CourseList.class;
-//                break;
-//            case R.id.nav_fav:
-//                //fragmentClass = .class;
-//                break;
-//            case R.id.nav_create_course:
-//                fragmentClass = CreateCourse.class;
-//                break;
-//            case R.id.nav_log_out:
-//                FirebaseAuth.getInstance().signOut();
-//                fragmentClass = LoginActivity.class;
-//                break;
-//        }
-//
-//        try {
-//            fragment = (Fragment) Class.newInstance();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        // Insert the fragment by replacing any existing fragment
-//        FragmentManager fragmentManager = getSupportFragmentManager();
-//        fragmentManager.beginTransaction().replace(R.id.fragment_container, new Fragment()).commit();
-//    }
-
-
-//    @Override
-//    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//        switch(item.getItemId()){
-//
-//            case R.id.nav_profile:
-//                Toast.makeText(getApplicationContext(), "Profile clicked", Toast.LENGTH_SHORT).show();
-//                Intent toProfile = new Intent(this, UserProfile.class);
-//                startActivity(toProfile);
-//
-//                break;
-//            case R.id.nav_courses:
-//                Intent toCourseList = new Intent(this, CourseList.class);
-//                startActivity(toCourseList);
-//                startActivity(toCourseList);
-//                break;
-//            case R.id.nav_my_courses:
-//                Toast.makeText(getApplicationContext(), "MyCourses clicked", Toast.LENGTH_SHORT).show();
-////                Intent toMyCourses = new Intent(this, MyCourses.class);
-////                startActivity(toMyCourses);
-////                finish();
-//                break;
-//            case R.id.nav_fav:
-//                Toast.makeText(getApplicationContext(), "Favourites clicked", Toast.LENGTH_SHORT).show();
-////                Intent toFavourites = new Intent(this, Favourites.class);
-////                startActivity(toFavourites);
-////                finish();
-//                break;
-//            case R.id.nav_create_course:
-//                Intent toCreateCourse = new Intent(this, CreateCourse.class);
-//                startActivity(toCreateCourse);
-//                break;
-//            case R.id.nav_log_out:
-//                FirebaseAuth.getInstance().signOut();
-//                Intent toLogin = new Intent(this, LoginActivity.class);
-//                startActivity(toLogin);
-//                break;
-//
-//        }
-//
-//        drawer.closeDrawer(GravityCompat.START);
-//        return true;//the item was selected*/
-//    }
-
 }
