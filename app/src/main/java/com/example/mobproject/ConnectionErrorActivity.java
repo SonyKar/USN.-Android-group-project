1a4209d6d1761d1043d412b8ff4938a71695c6af
CosminaL <cosmina_lucaci@yahoo.com>
2021-12-16 16:32:08 +0200

    - added connection error activity (test2)

---
+++ app/src/main/java/com/example/mobproject/ConnectionErrorActivity.java
@@ -0,0 +1,15 @@
+package com.example.mobproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ConnectionErrorActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.connection_error);
+
+
+    }
+}
