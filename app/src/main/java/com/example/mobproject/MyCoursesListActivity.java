25a667c2d53459763b0d7d08b1e63952bb5ba0b1
poirot-en-bardo <balica.eliza@gmail.com>
2021-12-16 17:13:51 +0100

    MyCourses showing the created courses for teachers and enrolled courses for students

--- app/src/main/java/com/example/mobproject/MyCoursesListActivity.java
+++ app/src/main/java/com/example/mobproject/MyCoursesListActivity.java
@@ -2,11 +2,9 @@ package com.example.mobproject;
 
 import android.content.Context;
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.example.mobproject.adapters.MyCoursesAdapter;
 import com.example.mobproject.constants.UserInfo;
 import com.example.mobproject.db.EnrolledCoursesDatabase;
@@ -20,16 +18,15 @@ import java.util.ArrayList;
 
 public class MyCoursesListActivity extends AppCompatActivity {
     private Context context;
-
-    private ArrayList<DocumentReference> favouriteList;
     private String userId;
+    private RecyclerView myCourseListRecyclerView;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_courses_list);
 
         context = this;
-
+        myCourseListRecyclerView = findViewById(R.id.my_courses_list);
         UserInfo userInfo = new UserInfo(this);
         userId = userInfo.getUserId();
 
@@ -44,26 +41,22 @@ public class MyCoursesListActivity extends AppCompatActivity {
 
     private void fillCourses() {
         EnrolledCoursesDatabase database = new EnrolledCoursesDatabase();
-        database.getItems(userId, recyclerViewCallback);
+        database.getItems(userId, new Callback<Course>() {
+            @Override
+            public void OnFinish(ArrayList<Course> courseList) {
+                myCourseListRecyclerView.setLayoutManager(new LinearLayoutManager(context));
+                FavouriteCoursesDatabase favouriteCoursesDatabase = new FavouriteCoursesDatabase();
+                favouriteCoursesDatabase.getItems(userId, new Callback<DocumentReference>() {
+                    @Override
+                    public void OnFinish(ArrayList<DocumentReference> favouriteList) {
+                        MyCoursesAdapter adapter = new MyCoursesAdapter(context, courseList,
+                                favouriteList, userId);
+                        myCourseListRecyclerView.setAdapter(adapter);
+                    }
+                });
+            }
+        });
     }
-
-    private final Callback<Course> recyclerViewCallback = new Callback<Course>() {
-        @Override
-        public void OnFinish(ArrayList<Course> enrolledCourseList) {
-            RecyclerView myCourseListRecyclerView = findViewById(R.id.my_courses_list);
-            myCourseListRecyclerView.setLayoutManager(new LinearLayoutManager(context));
-            FavouriteCoursesDatabase favouriteCoursesDatabase = new FavouriteCoursesDatabase();
-            favouriteCoursesDatabase.getItems(userId, new Callback<DocumentReference>() {
-                @Override
-                public void OnFinish(ArrayList<DocumentReference> arrayList) {
-                    favouriteList = arrayList;
-                    MyCoursesAdapter adapter = new MyCoursesAdapter(context, enrolledCourseList,
-                            favouriteList, userId);
-                    myCourseListRecyclerView.setAdapter(adapter);
-                }
-            });
-        }
-    };
 
     public void onBackPressed() {
         MenuDrawer.onBackHandler();
