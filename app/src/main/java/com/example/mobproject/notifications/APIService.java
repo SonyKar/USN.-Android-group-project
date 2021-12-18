14981c5c86926323989697cacd4bbf006dfa21c0
poirot-en-bardo <balica.eliza@gmail.com>
2021-12-18 13:18:39 +0100

    Removed FCM server authorization key for security reasons

--- app/src/main/java/com/example/mobproject/notifications/APIService.java
+++ app/src/main/java/com/example/mobproject/notifications/APIService.java
@@ -9,7 +9,7 @@ public interface APIService {
     @Headers(
             {
                     "Content-Type: application/json",
-                    "Authorization: key=AAAAThSGAZE:APA91bENGlaj5gUU4JwZZZbwfab8vUIxJc5ksZIQjgBfNdLP8P_dSGsxHKM7LMCmB7zVoRT6F4pnWb1cUJmknlwkJahWLPn4elaRaa_zyzirOwdp31Ro8DRuK7sGnynDWJE5R4qCKy5t"
+//                    "Authorization: key="
             }
     )
 
